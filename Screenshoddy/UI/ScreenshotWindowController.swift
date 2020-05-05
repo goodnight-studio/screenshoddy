c2908414149d78ad337c14d23f4dd3b9c8daaaee
Geof Crowl <rectangular@gmail.com>
2020-05-04 23:10:15 -0600

    Remove white space

--- Screenshoddy/UI/ScreenshotWindowController.swift
+++ Screenshoddy/UI/ScreenshotWindowController.swift
@@ -17,14 +17,12 @@ class ScreenshotWindowController: NSWindowController {
         let offsetX = round(Globals.screenVisibleFrame.width / 2) - round(ScreenshotWindow.defaultWidth / 2)
         let offsetY = round(Globals.screenVisibleFrame.height / 2) - round(ScreenshotWindow.defaultHeight / 2)
         
-        
         screenshotWindow = ScreenshotWindow(
             contentRect: NSRect(x: offsetX, y: offsetY, width: ScreenshotWindow.defaultWidth, height: ScreenshotWindow.defaultHeight),
             styleMask: [.resizable, .closable, .miniaturizable, .titled ], backing: .buffered, defer: false)
         
         window = screenshotWindow!
         window?.setFrameAutosaveName(screenshotWindow!.frameAutosaveName)
-        
     }
 
 }

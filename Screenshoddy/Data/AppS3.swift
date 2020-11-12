b622ff60dabfb21efe92c6d4047a878db7a2e7df
Geof <rectangular@gmail.com>
2020-11-11 22:38:42 -0700

    Create S3 singleton

---
+++ Screenshoddy/Data/AppS3.swift
@@ -0,0 +1,29 @@
+//
+//  AppS3.swift
+//  Screenshoddy
+//
+//  Created by Geof Crowl on 11/11/20.
+//  Copyright © 2020 Geof Crowl. All rights reserved.
+//
+
+import Foundation
+import S3
+
+class AppS3 {
+    
+    static var shared = AppS3()
+    
+    var s3 = S3(
+        accessKeyId: AppDefaults.s3AccessId,
+        secretAccessKey: AppKeychain.s3SecretKey
+    )
+    
+    func handleCredentialChange() {
+        self.s3 = S3(
+            accessKeyId: AppDefaults.s3AccessId,
+            secretAccessKey: AppKeychain.s3SecretKey
+        )
+    }
+    
+    private init() {}
+}
